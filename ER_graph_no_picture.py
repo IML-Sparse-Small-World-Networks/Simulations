f17a5583e1869b6bfdf526ec215772aa30e9e0b9
Andrew Shell <aashell2@illinois.edu>
2026-09-25 08:35:29 -0500

    standardized variables & automated data collection

--- ER_graph_no_picture.py
+++ ER_graph_no_picture.py
@@ -5,22 +5,23 @@ import matplotlib.pyplot as plt
 import Dijkstras_Algorithm as Dijkstras
 from datetime import datetime
 
-def main():
+def main(n_, l_, p_, lambda__, alpha_):
     # parameters
-    n = 1000
-    l = n/10
-    alpha = 5
-    p = alpha/n
-
-
-    now = datetime.now().second
+    
+    n = n_                              # number of vertices
+    l = l_                              # shortcut range
+    p = p_                              # probability of shortcut Binom dist
+    lambda_ = lambda__                  # new lambda var for alpha
+    alpha = alpha_                      # shortcut constant multiplier passed directly to main function
+    
 
     # random graph
+    now = datetime.now().second
     r = np.random.default_rng(seed=now)
     A = np.zeros((n, n), dtype=int)
 
     for  i in range(n):
-        j = (i + 1) % n
+        j = (i + 1) % n                 # underlying cycle
         A[i, j] = 1
         A[j, i] = 1
 
@@ -37,12 +38,12 @@ def main():
                     A[i, j] = 1
                     A[j, i] = 1
                     shortcuts.append((i, j))
+
     # weights
     rate = 1.0  # lambda
     scale = 1 / rate
 
     W = np.zeros((n, n), dtype=float)
-    mu = l * alpha/n
 
     for i in range(n):
         for j in range(i + 1, n):
@@ -52,7 +53,7 @@ def main():
                 if is_neighbor:
                     weight = r.exponential(scale=scale)
                 else:
-                    weight = mu * r.exponential(scale=scale)
+                    weight = alpha * r.exponential(scale=scale)
 
                 W[i, j] = weight
                 W[j, i] = weight
@@ -73,7 +74,7 @@ def main():
         cycle_edges.append((i, (i+1)%n))
 
 
-    plt.figure(figsize=(10, 10))
+    #plt.figure(figsize=(10, 10))
 
 
     graph_vertices = []
@@ -106,11 +107,50 @@ def main():
 
     print("len path:", len(path))
     print("num shortcut edges: ", shortcut_edges)
-    data = [n, l, p, len(shortcuts), sum(path_weights), len(path), shortcut_edges]
+
+    # record trial data
+    data = [n, l, p, lambda_, alpha, len(shortcuts), sum(path_weights), len(path), shortcut_edges]
     with open('data.csv', 'a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        
         writer.writerow(data)
 
 if __name__ == "__main__":
-    main()
+    n_ = 1000                            # number of vertices
+    l_ = n_/10                            # shortcut range
+    p_ = 5/n_                             # probability of shortcut Binom dist
+    lambda__ = 10                        # new lambda var for alpha
+    alpha_1 = (l_ * lambda__)/(2*l_-2)      # shortcut constant multiplier passed directly to main function
+    alpha_2 = (l_**2 * lambda__)/(2*l_-2)   # second alpha
+
+    with open('data.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow([
+            "n",
+            "l",
+            "p",
+            "lambda",
+            "alpha",
+            "shortcuts",
+            "path_weight",
+            "len_path",
+            "shortcuts_taken",
+            ])
+            writer.writerow([f"alpha = {alpha_1}", "", "", "", "", "", "", "", ""])
+
+    print("Starting alpha = (l * lambda_) / (2*l - 2)...")
+    for i in range(10):
+        print(f"Starting run {i + 1}...")
+        main(n_, l_, p_, lambda__, alpha_1)
+
+    with open('data.csv', mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow([f"alpha = {alpha_2}", "", "", "", "", "", "", "", ""])
+
+    print("Starting alpha = (l**2 * lambda_) / (2*l - 2)...")
+    for i in range(10):
+        print(f"Starting run {i + 1}...")
+        main(n_, l_, p_, lambda__, alpha_2)
+
+    print("finished!")
+
+    
